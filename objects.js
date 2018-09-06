9fddb99a043c87578e7465cf657294036d1a2cca
Omar Azzat <oazzat19@gmail.com>
2018-09-06 04:31:28 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,7 +1,7 @@
 var playlist = {U2:"Beautiful Day"}
 
 function updatePlaylist(playlist, artistName, songTitle){
-  playlist.artistName = songTitle
+  playlist[artistName] = songTitle
   return playlist
 }
 
